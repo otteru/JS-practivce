d5012d7b7c392c1664c3f8db08dcf0c8bad79053
otteru <otteru@naver.com>
2023-12-23 13:06:06 +0000

    added day30 content

---
+++ app.js
@@ -0,0 +1,9 @@
+let greetingText = "Hi I\'m Yudam";
+let age = 32;
+alert(greetingText);
+alert(age);
+
+let hobbies = ["coding", "movies", "basketball"];
+alert(hobbies[0]);
+
+let job = {title: "Developer", place: "Sachen", salary: 500}
