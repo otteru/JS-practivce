0061ac1654059bcc261bae51ccaf47bd1b0663ed
otteru <otteru@naver.com>
2023-12-24 10:36:14 +0000

    added day31 content

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-let greetingText = "Hi I\'m Yudam";
+let greetingText = "Hi I'm Yudam";
 let age = 32;
-alert(greetingText);
-alert(age);
-
 let hobbies = ["coding", "movies", "basketball"];
-alert(hobbies[0]);
+let job = {
+	title: "Developer", 
+	place: "Sachen", 
+	salary: 500
+};
 
-let job = {title: "Developer", place: "Sachen", salary: 500}
+let adultAges 
+
+function calculateAdultAges(userAge) {
+	return userAge - 18;
+}
+
+age = 20;
+adultAges = calculateAdultAges(age);
+alert(adultAges);
